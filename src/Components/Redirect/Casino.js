ca49841eab1b7c6558b02527341681cbf5f5f531
Nickz-dev <nick.apios@gmail.com>
2025-03-26 23:50:24 +0700

    prod

--- src/Components/Redirect/Casino.js
+++ src/Components/Redirect/Casino.js
@@ -4,33 +4,6 @@ import Loader from "../Loader/Loader";
 
 function Casino({ link }) {
     const [count, setCount] = useState(5);
-    const [visits, setVisits] = useState(null);
-
-    useEffect(() => {
-        const API_URL = "https://api.jsonbin.io/v3/b/6754a2d2ad19ca34f8d74471";
-        const API_KEY = "$2a$10$PvH8tIjkRk/gfW3h1uziKOVeQJVQiEdENDICCBHbg8nwkuASpqlVG";
-        fetch(API_URL, {
-            headers: {
-                "X-Master-Key": API_KEY,
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const currentVisits = data.record.visits;
-                setVisits(currentVisits);
-
-                // Увеличение счётчика
-                fetch(API_URL, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "X-Master-Key": API_KEY,
-                    },
-                    body: JSON.stringify({ visits: currentVisits + 1 }),
-                });
-            })
-            .catch((error) => console.error("Ошибка при работе с API:", error));
-    }, []);
 
     useEffect(() => {
         // Таймер обратного отсчёта
@@ -43,7 +16,7 @@ function Casino({ link }) {
         // Перенаправление по ссылке, если обратный отсчёт завершён
         if (count === 0) {
             const redirectTimer = setTimeout(() => {
-                window.location.href = link;
+                window.location.replace(link)
             }, 3000);
 
             return () => clearTimeout(redirectTimer); // Очистка таймера перенаправления
@@ -58,11 +31,6 @@ function Casino({ link }) {
                 <h2>Search Casino - {count} sec.</h2>
                 {count === 0 && <p>Зеркало найдено! Перенаправление...</p>}
             </CasinoCard>
-            {visits !== null && (
-                <CasinoCard>
-                    <p>Всего переходов: {visits}</p>
-                </CasinoCard>
-            )}
             <CasinoCard>
                 <Loader />
             </CasinoCard>
